570cc6ed375189ac3e32659016e83c24385866dc
Tomás Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2021-10-17 12:28:56 -0300

    fix: remove license builder from home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import React from "react";
-import styles from "../styles/Home.module.css";
-import Navbar from "../common/navbar";
 import { Typography } from "@mui/material";
-import LicenseBuilder from "../common/license-builder";
 
 const Home: NextPage = () => {
   return (
     <>
       <Typography>Content!</Typography>
-      <LicenseBuilder />
     </>
   );
 };
