aee61b6b0aff1af1f78e128fff8e007460c118df
Tomás Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2021-11-03 20:29:55 -0300

    feat: translate messages to spanish

--- spanish/error-messages.tsx
+++ spanish/error-messages.tsx
@@ -1,5 +1,22 @@
-import { OPPORTUNITY_NOT_FOUND } from "../common/error-messages";
+import * as Messages from "../common/error-messages";
 
 export function errorMessageToSpanish(errorMessage: string) {
-  return errorMessage;
+  switch (errorMessage) {
+    case Messages.OPPORTUNITY_NOT_FOUND:
+      return "No se encontró la oportunidad";
+    case Messages.DEVELOPMENT_DATA_MISSING:
+      return "Faltan cargar los datos de desarrollo";
+    case Messages.FIRST_MEETING_DATA_MISSING:
+      return "Faltan cargar los datos de primera reunión";
+    case Messages.INVALID_SOURCE_STEP:
+      return "No es posible avanzar al siguiente paso";
+    case Messages.NEGOTIATION_DATA_MISSING:
+      return "Faltan cargar los datos de negociación";
+    case Messages.POC_DEVELOPMENT_DATA_MISSING:
+      return "Faltan cargar los datos de desarrollo de POC";
+    case Messages.POC_IMPLEMENTATION_DATA_MISSING:
+      return "Faltan cargar los datos de implementación de POC";
+    case Messages.PROSPECT_DATA_MISSING:
+      return "Faltan cargar los datos de prospecto";
+  }
 }
